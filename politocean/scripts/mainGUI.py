b694a4a904d1190e722759c2a1f0fbee53f04a29
Detjon <34067851+Detjon13@users.noreply.github.com>
2018-05-09 18:12:30 +0200

    Update

--- politocean/scripts/mainGUI.py
+++ politocean/scripts/mainGUI.py
@@ -12,35 +12,32 @@ import sys
 from ROV import ROV
 from gui import Window
 from time import sleep
-from std_msgs.msg import String
 import timeit
-import thread
 import rospy
-import os
-import signal
 from errmess_publisher import *
 from politocean.msg import *
 
-#set the node name
-rospy.init_node(NODE.GUI, anonymous=False)
 
 #function to init ROV and send commands
 def initAndSend(window, rov):
-
-    #while to check if the ROV is awake, as well as tell the ROV that GUI is awake
+   
+    #while to check if the ROV is awake, as well as tell the ROV that GUI is aw$
     while not rospy.is_shutdown():
         sleep(0.5)
-        tic = timeit.default_timer()
+        tic = timeit.default_timer()   #tell the ROV that GUI is awake
         while not rospy.is_shutdown() and not rov.isAwake(): #if it's not awake
             sleep(0.1)
             if timeit.default_timer()-tic >= 5: #after a bit
                 tic = timeit.default_timer()
-                publishErrors(NODE.GUI, "ROV is not responding") #print on console
-                publishComponent(NODE.GUI, ID.ATMEGA, STATUS.DISABLED) #publish ATMega status
+                publishErrors(NODE.GUI, "ROV is not responding") #print on cons$
+                publishComponent(NODE.GUI, ID.ATMEGA, STATUS.DISABLED) #publish$
             sleep(0.1)
-            
 
 def main():
+
+    #set the node name
+    rospy.init_node(NODE.GUI, anonymous=False)    
+    
     app = QtGui.QApplication(sys.argv) #gui app init
 
     #window object
@@ -50,17 +47,17 @@ def main():
     #init rov object
     rov = ROV(window)
 
-    #start the gui app
+    #init a thread passing window object as argument
+    #try:
+        #thread.start_new_thread(initAndSend, (window, rov, ))
+    #except:
+        #print("Unable to start the commands thread. Exit")
+        #exit(-1)
+	#start the gui app
     n=app.exec_()
-    
-      #init a thread passing window object as argument
-    try:
-        thread.start_new_thread(initAndSend, (window, rov, ))
-    except:
-        print("Unable to start the commands thread. Exit")
-        exit(-1)      
-    
-    exit(n)
+
+    sys.exit(n)
 
 if __name__ == '__main__':
     main()
+
